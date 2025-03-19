94765bbcba7f2d1251672b646c8d12775c580b99
Tejas Kumar D <tejas.kumar1@hp.com>
2025-03-19 10:09:37 +0530

    Modified config files

--- checkout_api/procedural/config.py
+++ checkout_api/procedural/config.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB_CONFIG = {
-    "USER": "root",
-    "PASSWORD": "sHASHI&456",
-    "HOST": "localhost",
-    "DATABASE": "checkout_db",
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "DATABASE": os.getenv("DB_DATABASE"),
 }
