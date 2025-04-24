a4a4fe6b82ccb510820c807ccaa658be4cd708ae
Tejas Kumar D <tejas.kumar1@hp.com>
2025-04-24 18:49:51 +0530

    changed the logic to validate PIN before asking for withdrawal amount.

--- atm_simulaation/main.py
+++ atm_simulaation/main.py
@@ -3,29 +3,39 @@ from account import Account
 from service import ATMService
 from exceptions import ATMException, CardBlockedException
 
+
 def main():
     atm = ATM(total_cash=100000)
-    account = Account(pin="1234", balance=45000)
+    account = Account(pin="2134", balance=45000)
     service = ATMService(atm, account)
 
-    print("Welcome to Python ATM Simulation.\n")
+    print("💳 Welcome to Python ATM Simulation 💳\n")
 
     while True:
         try:
-            pin = input("Enter your 4-digit PIN: ")
+            pin = input("🔢 Enter your 4-digit PIN: ")
+            account.validate_pin(pin)
+            print("PIN validated successfully.\n")
+            break
+        except CardBlockedException as e:
+            print(f"{e}")
+            print("Your card has been blocked. Exiting.")
+            return
+        except ATMException as e:
+            print(f"{e}")
+        except Exception as e:
+            print(f"Unexpected Error: {e}")
+
+    while True:
+        try:
             amount_input = input("Enter withdrawal amount (₹): ")
 
             if not amount_input.isdigit():
                 raise ValueError("Withdrawal amount must be a number.")
 
             amount = int(amount_input)
-
             service.withdraw_money(pin, amount)
 
-        except CardBlockedException as e:
-            print(f"{e}")
-            print("Your card has been blocked. Exiting.")
-            break
         except ATMException as e:
             print(f"ATM Error: {e}")
         except ValueError as e:
@@ -35,9 +45,10 @@ def main():
 
         print("\nDo you want to perform another transaction?")
         choice = input("Type 'y' to continue or anything else to exit: ").lower()
-        if choice != 'y':
+        if choice != "y":
             print("Thank you for using Python ATM. Goodbye!")
             break
+
 
 if __name__ == "__main__":
     main()
